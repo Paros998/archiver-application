cd30b172063d5d6217ee8f4596b266215cba5243
Paros998 <patrykg198@gmail.com>
2024-01-13 18:49:16 +0100

    added profile to test

--- src/test/java/com/pd/archiver/ArchiverApplicationTests.java
+++ src/test/java/com/pd/archiver/ArchiverApplicationTests.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 /**
  * The type Archiver application tests.
  */
 @SpringBootTest
+@ActiveProfiles("devlocal")
 class ArchiverApplicationTests {
 
     @Autowired
